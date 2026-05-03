949fe3dc3c7f3f051afd4df70faaf661db2efd62
Luke3470 <ljcadman@outlook.com>
2026-05-03 19:04:20 +0100

    Base Set out Time for Physdo Code

--- src/main/java/uk/ac/mmu/enterpriseprogrammingrest/Controllers/RestBookServlet.java
+++ src/main/java/uk/ac/mmu/enterpriseprogrammingrest/Controllers/RestBookServlet.java
@@ -7,9 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("bookAPI")
+@WebServlet("/bookAPI")
 public class RestBookServlet extends HttpServlet {
-
+  @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
 
@@ -19,4 +19,28 @@ public class RestBookServlet extends HttpServlet {
     out.println("</body></html>");
   }
 
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+
+    PrintWriter out = response.getWriter();
+    out.println("<html><body>");
+    out.println("<h1>" +  "</h1>");
+    out.println("</body></html>");
+  }
+
+  @Override
+  public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+  }
+
+  @Override
+  public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+
+  }
 }
